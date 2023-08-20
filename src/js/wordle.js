4bd8bee2645b2de0c85c37d19fa57be321c51c42
Sean Byrd <byrd.sean1015@gmail.com>
2023-08-19 23:31:19 -0400

    Added code to detect valid keypress. Both via keyboard and clicking on html button.

--- src/js/wordle.js
+++ src/js/wordle.js
@@ -1,1 +1,41 @@
-console.log("Hello World!");
+const library = ["exact"];
+
+document.querySelectorAll(".key").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    keypress(e.target.value);
+  });
+});
+
+window.addEventListener("keydown", (e) => {
+  keypress(e.key);
+});
+
+const keypress = (value) => {
+  if (!value) return;
+
+  if (value.toUpperCase() === "ENTER") {
+    pressedEnter();
+    return;
+  }
+
+  if (value.toUpperCase() === "BACKSPACE") {
+    pressedBackspace();
+    return;
+  }
+
+  if (value.length !== 1) return;
+  const upperValue = value.toUpperCase();
+
+  const asciiA = "A".charCodeAt(0);
+  const asciiZ = "Z".charCodeAt(0);
+  const asciiUpperValue = upperValue.charCodeAt(0);
+  if (asciiUpperValue < asciiA || asciiZ < asciiUpperValue) return;
+
+  validateValue(upperValue);
+};
+
+const pressedEnter = () => {};
+
+const pressedBackspace = () => {};
+
+const validateValue = (value) => {};
