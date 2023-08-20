5c00da6f238d5baaa98c4ec31455e0d5c700a1db
Sean Byrd <byrd.sean1015@gmail.com>
2023-08-20 00:43:37 -0400

    Added logic to validate a single line against the word.

--- src/js/wordle.js
+++ src/js/wordle.js
@@ -1,4 +1,8 @@
-const library = ["exact"];
+const library = ["event"];
+const lines = document.querySelectorAll(".line");
+let currentLineIndex = 0;
+let currentWord;
+let currentWordMetrics;
 
 document.querySelectorAll(".key").forEach((element) => {
   element.addEventListener("click", (e) => {
@@ -10,16 +14,37 @@ window.addEventListener("keydown", (e) => {
   keypress(e.key);
 });
 
+const getCurrentWord = () => {
+  const index = Math.floor(Math.random() * library.length);
+  return library[index].toUpperCase();
+};
+
+const setWordMetrics = (word) => {
+  const metrics = new Map();
+  if (!word) return metrics;
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word.charAt(i);
+    const letterMetric = metrics.get(letter);
+    if (letterMetric) {
+      metrics.set(letter, [...letterMetric, i]);
+    } else {
+      metrics.set(letter, [i]);
+    }
+  }
+  return metrics;
+};
+
 const keypress = (value) => {
   if (!value) return;
 
   if (value.toUpperCase() === "ENTER") {
-    pressedEnter();
+    validateValue();
     return;
   }
 
   if (value.toUpperCase() === "BACKSPACE") {
-    pressedBackspace();
+    removeLetter();
     return;
   }
 
@@ -31,11 +56,60 @@ const keypress = (value) => {
   const asciiUpperValue = upperValue.charCodeAt(0);
   if (asciiUpperValue < asciiA || asciiZ < asciiUpperValue) return;
 
-  validateValue(upperValue);
+  addLetter(upperValue);
 };
 
-const pressedEnter = () => {};
+const getCurrentLineLetters = () => {
+  const currentLine = lines[currentLineIndex];
+  return Array.from(currentLine.querySelectorAll(".letter"));
+};
 
-const pressedBackspace = () => {};
+const removeLetter = () => {
+  const toRemove = getCurrentLineLetters()
+    .reverse()
+    .find((letter) => letter.innerHTML?.length !== 0);
+  if (!toRemove) return;
+  toRemove.innerHTML = "";
+};
 
-const validateValue = (value) => {};
+const addLetter = (value) => {
+  const nextLetter = getCurrentLineLetters().find(
+    (letter) => letter.innerHTML?.length === 0
+  );
+  if (!nextLetter) return;
+  nextLetter.innerHTML = value;
+};
+
+const validateValue = () => {
+  const letters = getCurrentLineLetters();
+
+  const emptyLetter = letters.find((letter) => letter.innerHTML?.length === 0);
+  if (emptyLetter) {
+    //Show not enough letter message
+    console.log("Not enough letters!");
+    return;
+  }
+
+  letters.forEach((letter, index) => {
+    const letterValue = letter.innerHTML;
+    const letterMetric = currentWordMetrics.get(letterValue) || [];
+
+    if (letterMetric.length === 0) {
+      letter.classList.add("not-found");
+      return;
+    }
+
+    const matchedIndex = letterMetric.find((metric) => metric === index);
+    if (matchedIndex !== undefined) {
+      letter.classList.add("correct-location");
+    } else {
+      letter.classList.add("incorrect-location");
+    }
+  });
+};
+
+const startGame = () => {
+  currentWord = getCurrentWord();
+  currentWordMetrics = setWordMetrics(currentWord);
+};
+startGame();
